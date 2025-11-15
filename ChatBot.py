f3e4fd20a1c9eaa87b8a2f2dd12377b1d7a064f2
Gibber17 <gfrancis1707@gmail.com>
2025-11-15 15:55:54 -0500

    Refactor sendMessage method in ChatBot class
    Updated sendMessage method to include 'self' parameter and return response text instead of printing.

--- ChatBot.py
+++ ChatBot.py
@@ -3,8 +3,8 @@ from google import genai
 client = genai.Client(api_key="AIzaSyAmegGOYvBn_jsnrZjW6OAdENgt-j1qQ-A")
 
 class ChatBot:
-    def sendMessage(userMessage):
+    def sendMessage(self, userMessage):
         response = client.models.generate_content(
             model="gemini-2.5-flash", contents=userMessage
         )
-        print(response.text)
+        return response.text
