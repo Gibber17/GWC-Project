0d5e4e44f2fbd8c0a6bc1799314408ae2d0e5cae
Gibber17 <gfrancis1707@gmail.com>
2025-11-15 14:07:04 -0500

    Implement ChatBot class for message handling
    Added ChatBot class to encapsulate message sending functionality.

--- ChatBot.py
+++ ChatBot.py
@@ -2,7 +2,9 @@ from google import genai
 
 client = genai.Client(api_key="AIzaSyAmegGOYvBn_jsnrZjW6OAdENgt-j1qQ-A")
 
-response = client.models.generate_content(
-    model="gemini-2.5-flash", contents="In one short paragraph. Explain how AI works in a few words"
-)
-print(response.text)
+class ChatBot:
+    def sendMessage(userMessage):
+        response = client.models.generate_content(
+            model="gemini-2.5-flash", contents=userMessage
+        )
+        print(response.text)
